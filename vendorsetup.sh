04c47d89beac35faf87cdb597293d0ffd3befb80
Md Alam <151665023+mdalam073@users.noreply.github.com>
2024-09-07 12:34:15 +0600

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
 # Clone own audio config tree
-rm -rf hardware/qcom-caf/sm8350/audio/configs/holi && git clone https://github.com/veux-frost-testzone/hardware_qcom-caf_sm8350_audio_configs_holi.git -b dolby hardware/qcom-caf/sm8350/audio/configs/holi --depth=1
+rm -rf hardware/qcom-caf/msm8953/audio/configs/holi && git clone https://github.com/mdalam073/hardware_qcom-caf_msm8953_audio_configs_holi.git -b dolby hardware/qcom-caf/sm8350/audio/configs/holi --depth=1
